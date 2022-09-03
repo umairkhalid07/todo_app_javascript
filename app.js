800027e6cb673f139f81d2272f60cd996e0f674a
umairkhalid07 <umairk911.uk@gmail.com>
2022-09-03 17:59:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const addForm = document.querySelector('.add');
+const search = document.querySelector('.search input');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+  list.innerHTML += html;
+};
+
+const filterTodos = term => {
+  Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+list.addEventListener('click', e => {
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+});
+
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
